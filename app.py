3c522cc26ce6ecc4db735314fdd63ac98101fc6b
amurad77 <amurad799@gmail.com>
2021-06-03 12:46:57 +0400

    venom

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:welcome123@127.0.0.1:3306/Venom'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:welcome123@db:3306/Venom'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'twqfygdhugejfdskvmdf'
 
